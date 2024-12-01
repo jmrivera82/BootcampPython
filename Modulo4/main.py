86d4c1e448c323a12acc294a9e4ec15a587e0de6
jmrivera82 <jmrivera82@gmail.com>
2024-12-01 18:22:35 -0300

    Modulo 4 parte 1

--- Modulo4/main.py
+++ Modulo4/main.py
@@ -6,8 +6,8 @@ def ingresar_datos():
 
         print(f"Datos del automovil:", indice+1)
 
-        auto = Automovil.ingresar_automovil()
-        datos_vehiculos.append(auto)
+        autos = Automovil.ingresar_automovil()
+        datos_vehiculos.append(autos)
 
 print("PRACTICA DE CONSOLIDACIÓN MODULO 4")
 print("**********************************")
@@ -16,4 +16,11 @@ datos_vehiculos=[]
 
 num_vehiculos=int(input(f"Cuantos vehículos desea ingresar: "))
 ingresar_datos()
-print(datos_vehiculos)
+#print(datos_vehiculos) -- prueba de impresión de los vehiculos ingresados
+
+print(f"Imprimiendo por pantalla los vehículos: ")
+#error AttributeError: 'tuple' object has no attribute 'imprimir_automovil'
+for autito in datos_vehiculos:
+    autito.imprimir_automovil(self)
+
+
--- Modulo4/practica.py
+++ Modulo4/practica.py
@@ -18,11 +18,11 @@ class Automovil:
     def ingresar_automovil():
         marca=input(f"Ingrese Marca: ")
         modelo=input(f"Ingrese Modelo: ")
-        numeroDeRuedas=input(f"Ingrese Numero de ruedas: ")
-        velocidad=input(f"Ingrese velocidad: ")
-        cilindrada=input(f"Ingrese cilindrada: ")
-        return marca,modelo,numeroDeRuedas,velocidad,cilindrada
+        numero_de_ruedas=input(f"Ingrese Numero de ruedas: ")
+        velocidad=input(f"Ingrese velocidad en km/h: ")
+        cilindrada=input(f"Ingrese cilindrada en cc : ")
+        return marca,modelo,numero_de_ruedas,velocidad,cilindrada
 
     def imprimir_automovil(self):
-        return f"Datos del automovil: Marca: {self.marca}, Modelo: {self.modelo}"
+        print(f"Datos del automovil: Marca {self.marca}, Modelo {self.modelo}, {self.numero_de_ruedas} Ruedas, {self.velocidad} Km/h, {self.cilindrada} cc")
 
