8e4be41e8389a0fc298cf7bfbf2b4637150e3e23
jmrivera82 <jmrivera82@gmail.com>
2024-12-06 01:08:24 -0300

    actualizacion al 06-12

--- Modulo4/main.py
+++ Modulo4/main.py
@@ -2,25 +2,29 @@ from practica import Vehiculo, Automovil
 
 def ingresar_datos():
 
+    datos_vehiculos=[]
+
     for indice in range(num_vehiculos):
+       print(f"Datos del automovil:", indice+1)
 
-        print(f"Datos del automovil:", indice+1)
+       autos = Automovil.ingresar_automovil()
+       datos_vehiculos.append(autos)
 
-        autos = Automovil.ingresar_automovil()
-        datos_vehiculos.append(autos)
+       print(f"Imprimiendo por pantalla los vehículos: ")
+
+       for autito in datos_vehiculos:
+            #autito.imprimir_automovil()
+            print(datos_vehiculos)
+
+
+#Programa
 
 print("PRACTICA DE CONSOLIDACIÓN MODULO 4")
 print("**********************************")
-
-datos_vehiculos=[]
-
 num_vehiculos=int(input(f"Cuantos vehículos desea ingresar: "))
 ingresar_datos()
 #print(datos_vehiculos) -- prueba de impresión de los vehiculos ingresados
 
-print(f"Imprimiendo por pantalla los vehículos: ")
-#error AttributeError: 'tuple' object has no attribute 'imprimir_automovil'
-for autito in datos_vehiculos:
-    autito.imprimir_automovil(self)
+
 
 
--- Modulo4/practica.py
+++ Modulo4/practica.py
@@ -1,17 +1,17 @@
 
 class Vehiculo:
 
-    def __init__(self, marca, modelo, numeroDeRuedas):
+    def __init__(self, marca, modelo, numero_de_ruedas):
         self.marca=marca
         self.modelo=modelo
-        self.numeroDeRuedas=numeroDeRuedas
+        self.numero_de_ruedas=numero_de_ruedas
 
 class Automovil:
 
-    def __init__(self):
-        self.marca=""
-        self.modelo=""
-        self.numeroDeRuedas=numeroDeRuedas
+    def __init__(self,marca,modelo,numero_de_ruedas,velocidad,cilindrada):
+        self.marca=marca
+        self.modelo=modelo
+        self.numero_de_ruedas=numero_de_ruedas
         self.velocidad=velocidad
         self.cilindrada=cilindrada
 
@@ -26,3 +26,13 @@ class Automovil:
     def imprimir_automovil(self):
         print(f"Datos del automovil: Marca {self.marca}, Modelo {self.modelo}, {self.numero_de_ruedas} Ruedas, {self.velocidad} Km/h, {self.cilindrada} cc")
 
+
+#prueba instanciando la clase Automovil
+
+#automovil1=Automovil("aa","bb",4,180,50)
+#automovil2=Automovil("cc","aa",4,200,50)
+#automovil3=Automovil("tt","rr",4,220,50)
+
+#automovil1.imprimir_automovil()
+#automovil2.imprimir_automovil()
+#automovil3.imprimir_automovil()
