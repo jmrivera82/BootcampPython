582cd8906d03f34d6ed0eb0b4862f210d58eae4c
jmrivera82 <jmrivera82@gmail.com>
2024-12-11 02:54:11 -0300

    actualizacion modulo6

--- Modulo6/proyecto/proyecto_vehiculos_django/vehiculo/models.py
+++ Modulo6/proyecto/proyecto_vehiculos_django/vehiculo/models.py
@@ -5,10 +5,26 @@ from django.db import models
 class Vehiculo(models.Model):
     pass
     #revisar choices para marca y categoria
+
+    MARCA_CHOICES=[
+        ('FIAT','Fiat'),
+        ('CHEVROLET','Chevrolet'),
+        ('FORD','Ford'),
+        ('TOYOTA','Toyota')
+       ]
+    
+    marca=models.CharField(max_length=20, choices=MARCA_CHOICES,default='Ford')
     modelo=models.CharField(max_length=100)
     serial_carroceria=models.CharField(max_length=50)
     serial_motor=models.CharField(max_length=50)
 
+    CATEGORIA_CHOICES=[
+        ('PARTICULAR','Particular'),
+        ('TRANSPORTE','Transporte'),
+        ('CARGA','Carga'),
+       ]
+    
+    categoria=models.CharField(max_length=20,choices=CATEGORIA_CHOICES,default='Particular')
     precio=models.IntegerField()
     fecha_de_creacion=models.DateField()
     fecha_de_modificacion=models.DateField()
