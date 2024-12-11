e616349b94120aca60d267263af047dca4799b01
jmrivera82 <jmrivera82@gmail.com>
2024-12-11 02:30:51 -0300

    actualizacion modulo6

--- Modulo6/proyecto/proyecto_vehiculos_django/vehiculo/models.py
+++ Modulo6/proyecto/proyecto_vehiculos_django/vehiculo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Vehiculo(models.Model):
+    pass
+    #revisar choices para marca y categoria
+    modelo=models.CharField(max_length=100)
+    serial_carroceria=models.CharField(max_length=50)
+    serial_motor=models.CharField(max_length=50)
+
+    precio=models.IntegerField()
+    fecha_de_creacion=models.DateField()
+    fecha_de_modificacion=models.DateField()
--- Modulo6/proyecto/proyecto_vehiculos_django/vehiculo/urls.py
+++ Modulo6/proyecto/proyecto_vehiculos_django/vehiculo/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from . import views
 
-from . import views    
+app_name="vehiculo"
 
-
-urlpatterns = [
-    path('', views.index, name='index'),
+urlpatterns=[
+    path('', views.index, name="index"),
 ]
