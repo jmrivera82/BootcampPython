a8968feac30c7f4ecbb2a3e0c473a1296629a180
jmrivera82 <jmrivera82@gmail.com>
2025-01-16 00:26:05 -0300

    Actualizacion practica 7 parte 2

--- Modulo7/practica_final_orm_django/laboratorio/admin.py
+++ Modulo7/practica_final_orm_django/laboratorio/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Laboratorio, DirectorGeneral, Producto
+
+
+
+class LaboratorioAdmin(admin.ModelAdmin):
+    fields=['nombre']
+    list_display=('id','nombre')
+    list_display_links=['nombre']
+    
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    fields=['nombre','laboratorio']
+    list_display=('id','nombre','laboratorio')
+    list_display_links=['nombre','laboratorio']
+    ordering=('nombre','laboratorio')
+
+class ProductoAdmin(admin.ModelAdmin):
+    fields=['nombre','laboratorio','f_fabricacion','p_costo','p_venta']
+    list_display=('id','nombre','laboratorio')
+    list_display_links=['nombre','laboratorio']
+    ordering=('nombre','laboratorio')
+    list_filter=('nombre','laboratorio')
+    list_per_page=4
+
+admin.site.register(Laboratorio, LaboratorioAdmin)
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
--- Modulo7/practica_final_orm_django/laboratorio/models.py
+++ Modulo7/practica_final_orm_django/laboratorio/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from datetime import date
+
 # Create your models here.
 class Laboratorio(models.Model):
     nombre=models.CharField(max_length=255)
 
     def __str__(self):
-        return f"Nombre: {self.nombre}"
+        return f"{self.nombre}"
 
     class Meta:
         db_table='laboratorio'
@@ -13,10 +15,23 @@ class Laboratorio(models.Model):
 
 class DirectorGeneral(models.Model):
     nombre=models.CharField(max_length=255)
-    laboratorio=models.ForeignKey(Laboratorio,on_delete=models.SET_NULL, blank=True,null=True)     
+    laboratorio=models.OneToOneField(Laboratorio,on_delete=models.CASCADE,related_name='director_general')     
     
     def __str__(self):
-        return f"Nombre: {self.nombre}"
+        return f"Nombre:{self.nombre}"
     
     class Meta:
         db_table='director_general'
+
+class Producto(models.Model):
+    nombre=models.CharField(max_length=255)
+    laboratorio=models.ForeignKey(Laboratorio,on_delete=models.SET_NULL, blank=True,null=True)     
+    f_fabricacion=models.DateField(default=date(2015,1,1))
+    p_costo=models.DecimalField(max_digits=12,decimal_places=2)
+    p_venta=models.DecimalField(max_digits=12,decimal_places=2)
+
+    def __str__(self):
+        return f"{self.nombre},{self.p_venta}"
+
+    class Meta:
+        db_table='productos'
