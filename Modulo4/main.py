5e9a8dceac2f97cd7229df5be731b589e075e5fe
jmrivera82 <jmrivera82@gmail.com>
2025-01-16 11:41:30 -0300

    actualización practica 4 parte 2 instancias

--- Modulo4/main.py
+++ Modulo4/main.py
@@ -19,3 +19,14 @@ print(f"Marca: {particular.marca}, Modelo: {particular.modelo}, {particular.nume
 print(f"Marca: {carga.marca}, Modelo: {carga.modelo}, {carga.numero_de_ruedas} ruedas, {carga.velocidad} km/h, {carga.cilindrada} cc, Carga: {carga.carga}")
 print(f"Marca: {bicicleta.marca}, Modelo: {bicicleta.modelo}, {bicicleta.numero_de_ruedas} ruedas, Tipo: {bicicleta.tipo_bicicleta}")
 print(f"Marca: {motocicleta.marca}, Modelo: {motocicleta.modelo}, {motocicleta.numero_de_ruedas} ruedas, Tipo: {motocicleta.tipo_bicicleta}, Motor: {motocicleta.motor}, Cuadro: {motocicleta.cuadro}, Nro radios: {motocicleta.nro_radios}")
+
+print("*"*40)
+print("Verificación de instancias")
+print("*"*40)
+
+print("Motocicleta es instancia con relación a Vehículo:",isinstance(motocicleta,Vehiculo))
+print("Motocicleta es instancia con relación a Automóvil:",isinstance(motocicleta,Automovil))
+print("Motocicleta es instancia con relación a Vehículo particular:",isinstance(motocicleta,Particular))
+print("Motocicleta es instancia con relación a Vehículo de carga:",isinstance(motocicleta,Carga))
+print("Motocicleta es instancia con relación a Bicicleta:",isinstance(motocicleta,Bicicleta))
+print("Motocicleta es instancia con relación a Motocicleta:",isinstance(motocicleta,Motocicleta))
