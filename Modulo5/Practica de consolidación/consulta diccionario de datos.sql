88ec4ffd98b3f53ff2c0e81fae63d8c4bd3cde0f
jmrivera82 <jmrivera82@gmail.com>
2025-01-17 18:36:37 -0300

    Actualización practica modulo 5 consulta diccionario de datos

--- Modulo5/Practica de consolidación/consulta diccionario de datos.sql
+++ Modulo5/Practica de consolidación/consulta diccionario de datos.sql
@@ -1,14 +1,15 @@
 SELECT
-    table_name,
-    column_name,
-    data_type,
-    character_maximum_length,
-    is_nullable
+    table_name AS nombre_tabla,
+    column_name AS nombre_columna,
+    data_type AS tipo_dato,
+    character_maximum_length AS longitud,
+    is_nullable AS columna_nulo,
+    domain_name AS columna_dominio,
+    column_default AS columna_defecto
 FROM
-    public.columns
+    information_schema.columns
 WHERE
     table_schema = 'public'  
     AND table_name IN ('customer','actor','category','film','film_actor','film_category','address','city','country','inventory','language','payment','rental','staff','store')  
 ORDER BY
-    table_name,
-    ordinal_position;
+    table_name;
