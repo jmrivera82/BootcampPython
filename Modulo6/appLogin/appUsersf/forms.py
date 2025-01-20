33d6967f2227620a9620ad00dfda58d5c1b2fe39
jmrivera82 <jmrivera82@gmail.com>
2025-01-20 03:50:46 -0300

    Actualización práctica Modulo 6 - Login, permisos, parte 4

--- Modulo6/appLogin/appUsersf/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class LoginForm (AuthenticationForm): #clase que hereda de AuthenticationForm
-    #atributos
-    username = forms.CharField(label="Usuario")
-    password = forms.CharField(widget=forms.PasswordInput, label="Contraseña")
